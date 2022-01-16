0b06f1ef76a698ce7c834ebd83673275f25c8b7c
nayeftechno <nayeftechno@gmail.com>
2022-01-16 16:57:41 +0200

    Commit changes

--- src/components/List.js
+++ src/components/List.js
@@ -1,7 +1,7 @@
 import { useEmployeeStore } from '../contexts/EmployeeContext';
 import { observer } from 'mobx-react-lite';
 function List() {
-    const { getEmployees, getLoading } = useEmployeeStore();
+    const { getEmployees, getLoading, setChecked, deleteEmployee } = useEmployeeStore();
     function renderLoading() {
         return (<li className='list-group-item' style={{ textAlign: 'center' }}>Loading....</li>);
     };
@@ -10,7 +10,18 @@ function List() {
             return (<>
                 {
                     getEmployees.map(({ id, name, exist }) => {
-                        return (<li className='list-group-item' key={id}>{id} - {name}</li>);
+                        return (<li className='list-group-item' key={id}>
+                            <span>{id} - {name}</span>
+                            <div style={{ float: 'right', display: 'flex', alignItems: 'center', width: 60, justifyContent: 'space-between' }}>
+                                <div>
+                                    <input type={'checkbox'} checked={exist} onChange={() => { setChecked({ id, name, exist }) }} />
+                                </div>
+                                <div>||</div>
+                                <div>
+                                    <a style={{ cursor: 'pointer' }} onClick={() => { deleteEmployee({ id, name, exist }) }}>X</a>
+                                </div>
+                            </div>
+                        </li>);
                     })
                 }
             </>);
---
+++ src/components/Statics.js
@@ -0,0 +1,14 @@
+import { observer } from 'mobx-react-lite';
+import { useEmployeeStore } from '../contexts/EmployeeContext';
+function Statics() {
+    const { getTotal, getExists, getNotExists } = useEmployeeStore();
+    return (<div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column' }}>
+        <h6>Total : {getTotal}</h6>
+        <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
+            <h6>Exists : {getExists}</h6>
+            <h6>{' '}&nbsp;&nbsp;||{' '}&nbsp;&nbsp;</h6>
+            <h6>NotExists : {getNotExists}</h6>
+        </div>
+    </div>);
+};
+export default observer(Statics);
